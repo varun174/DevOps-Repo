592ed2c33e61ed27aedab2af133d0459d74420f6
varun174 <42437402+varun174@users.noreply.github.com>
2024-12-06 10:16:23 +0530

    Update 2.sql

--- varun/2.sql
+++ varun/2.sql
@@ -3,7 +3,3 @@ UPDATE employees
 SET hire_date = '2021-06-01'
 WHERE last_name = 'Smith';
 
--- Select updated records
-SELECT id, first_name, last_name, hire_date
-FROM employees
-WHERE last_name = 'Smith';
